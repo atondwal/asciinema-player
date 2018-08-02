8cccf9ead9ee97140cacb866a37792d9c70db122
Anish Tondwalkar <twilightrook@gmail.com>
2018-08-02 08:09:04 -0400

    Add LibreJS wrapper to project.clj

--- project.clj
+++ project.clj
@@ -79,6 +79,7 @@
                                                             :provides ["asciinema.vt.codepoint-polyfill"]}]
                                             :optimizations :advanced
                                             :elide-asserts true
+                                            :output-wrapper "// @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache-2.0\n%s\n// @license-end"
                                             :source-map "resources/public/js/asciinema-player.js.map"
                                             :pretty-print  false}}}}
 
